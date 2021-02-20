e5a7cfccdd2fa9f568ef9394f1acb96a476e6669
ccdale <chris.allison@hivehome.com>
2021-02-20 04:40:39 +0000

    readme and test app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sdjson project
+# ccasdtv project
+
+Currently in development.
+
+## Install
+```
+# install dependencies
+poetry install
+# create a config file
+touch ~/.config/ccasdtv.yaml
+# Run the app with
+poetry run python sdjson/sd.py
+```
+
+## Status
+currently will obtain a SD API token and cache it for 23 hours in the config file, along with your sha1 hashed password.
---
+++ sdjson/sd.py
@@ -0,0 +1,46 @@
+"""Routines to interact with Schedules Direct"""
+
+import hashlib
+import json
+
+import requests
+
+from sdjson.config import readConfig
+from sdjson.config import writeConfig
+from sdjson.sdapi import SDApi
+from sdjson import __version__
+
+appname = "ccasdtv"
+cfgdirty = False
+
+cfg = readConfig(appname=appname)
+if "username" not in cfg:
+    cfg["username"] = input("Username: ")
+    cfgdirty = True
+if "password" not in cfg:
+    password = input("Password: ")
+    cfg["password"] = hashlib.sha1(password.encode()).hexdigest()
+    cfgdirty = True
+
+
+token = cfg["token"] if "token" in cfg else None
+tokenexpires = cfg["tokenexpires"] if "tokenexpires" in cfg else 0
+sd = SDApi(
+    cfg["username"],
+    cfg["password"],
+    appname=appname,
+    token=token,
+    tokenexpires=tokenexpires,
+    debug=True,
+)
+
+sd.apiStatus()
+msg = "OK" if sd.status else "NOK"
+print(f"{msg}: {sd.statusmsg}")
+if cfg["token"] != sd.token:
+    cfg["token"] = sd.token
+    cfg["tokenexpires"] = sd.tokenexpires
+    cfgdirty = True
+
+if cfgdirty:
+    writeConfig(cfg, appname=appname)
