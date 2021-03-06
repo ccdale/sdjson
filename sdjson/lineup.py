21a9d9114ca1ac6a50be8966b6c4aeedb4df76ec
ccdale <chris.allison@hivehome.com>
2021-03-06 03:13:27 +0000

    there isn't any need to make lineup a class

--- sdjson/lineup.py
+++ sdjson/lineup.py
@@ -21,72 +21,58 @@ import sys
 
 import ccalogging
 
-from sdjson.sdapi import SDApi
 
 log = ccalogging.log
 
-# TODO continue turning this into a class
-class SDLineup(SDApi):
-    """Schedules Direct Lineup class for the ccasdtv application."""
 
-    def __init__(self, ludata, **kwargs):
-        try:
-            super().__init__(**kwargs)
-        except Exception as e:
-            exci = sys.exc_info()[2]
-            lineno = exci.tb_lineno
-            fname = exci.tb_frame.f_code.co_name
-            ename = type(e).__name__
-            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
-            log.error(msg)
-            raise
+def parseStations(sdata, channums):
+    try:
+        channelsbyname = {}
+        channelsbyid = {}
+        for station in sdata:
+            station["channelnumber"] = channums[station["stationID"]]
+            channelsbyname[station["name"]] = station
+            channelsbyid[station["stationID"]] = station
+        return {"channelsbyname": channelsbyname, "channelsbyid": channelsbyid}
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+        log.error(msg)
+        raise
 
-    def parseStations(self, sdata, channums):
-        try:
-            channelsbyname = {}
-            channelsbyid = {}
-            for station in sdata:
-                station["channelnumber"] = channums[station["stationID"]]
-                channelsbyname[station["name"]] = station
-                channelsbyid[station["stationID"]] = station
-            return {"channelsbyname": channelsbyname, "channelsbyid": channelsbyid}
-        except Exception as e:
-            exci = sys.exc_info()[2]
-            lineno = exci.tb_lineno
-            fname = exci.tb_frame.f_code.co_name
-            ename = type(e).__name__
-            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
-            log.error(msg)
-            raise
 
-    def parseChannelMap(self, mapdata):
-        try:
-            stations = {}
-            for chan in mapdata:
-                stations[chan["stationID"]] = chan["channel"]
-            return stations
-        except Exception as e:
-            exci = sys.exc_info()[2]
-            lineno = exci.tb_lineno
-            fname = exci.tb_frame.f_code.co_name
-            ename = type(e).__name__
-            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
-            log.error(msg)
-            raise
+def parseChannelMap(mapdata):
+    try:
+        stations = {}
+        for chan in mapdata:
+            stations[chan["stationID"]] = chan["channel"]
+        return stations
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+        log.error(msg)
+        raise
 
-    def parseLineupData(self, jdata):
-        try:
-            channums = channeldict = None
-            if "map" in jdata:
-                channums = parseChannelMap(jdata["map"])
-            if channums is not None and "stations" in jdata:
-                channeldict = parseStations(jdata["stations"], channums)
-            return channeldict
-        except Exception as e:
-            exci = sys.exc_info()[2]
-            lineno = exci.tb_lineno
-            fname = exci.tb_frame.f_code.co_name
-            ename = type(e).__name__
-            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
-            log.error(msg)
-            raise
+
+def parseLineupData(jdata):
+    try:
+        channums = channeldict = None
+        if "map" in jdata:
+            channums = parseChannelMap(jdata["map"])
+        if channums is not None and "stations" in jdata:
+            channeldict = parseStations(jdata["stations"], channums)
+        return channeldict
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+        log.error(msg)
+        raise
