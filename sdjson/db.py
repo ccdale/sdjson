21b5464587c12d7687b9cb8d5ea983e95dac6603
ccdale <chris.allison@hivehome.com>
2021-04-04 10:59:14 +0100

    use IntegrityError to decide if the row already exists or not

--- sdjson/db.py
+++ sdjson/db.py
@@ -97,3 +97,25 @@ class SDDb(Base):
             msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
             log.error(msg)
             raise
+
+    def insertSql(self, sql, values):
+        try:
+            self.getConnection()
+            with self.connection:
+                log.debug(f"sql: {sql} values: {values}")
+                c = self.connection.cursor()
+                c.execute(sql, values)
+                log.debug(f"sql result: {rows}")
+            self.connection.close()
+            return True
+        except sqlite3.IntegrityError:
+            log.debug("Row already exists")
+            return False
+        except Exception as e:
+            exci = sys.exc_info()[2]
+            lineno = exci.tb_lineno
+            fname = exci.tb_frame.f_code.co_name
+            ename = type(e).__name__
+            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+            log.error(msg)
+            raise
