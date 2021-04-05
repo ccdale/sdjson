f5b30b7e46d93348dfd66ff868c3789ee6e5a89e
ccdale <chris.allison@hivehome.com>
2021-04-05 08:41:30 +0100

    select sql function

--- sdjson/db.py
+++ sdjson/db.py
@@ -119,3 +119,24 @@ class SDDb(Base):
             msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
             log.error(msg)
             raise
+
+    def selectSql(self, sql, values):
+        try:
+            rows = None
+            self.getConnection()
+            with self.connection:
+                log.debug(f"sql: {sql} values: {values}")
+                c = self.connection.cursor()
+                c.execute(sql, values)
+                rows = c.fetchall()
+                log.debug(f"sql result: {rows}")
+            self.connection.close()
+            return rows
+        except Exception as e:
+            exci = sys.exc_info()[2]
+            lineno = exci.tb_lineno
+            fname = exci.tb_frame.f_code.co_name
+            ename = type(e).__name__
+            msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+            log.error(msg)
+            raise
